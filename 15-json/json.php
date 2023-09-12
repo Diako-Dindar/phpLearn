8fa85bdb3f0757224a2e83e9e3e3e4302d43bd07
Diako-Dindar <diako.dindar@gmail.com>
2023-09-12 14:26:02 +0300

    edit the json file

--- 15-json/json.php
+++ 15-json/json.php
@@ -6,12 +6,15 @@ $person = [
     "age"    =>37,
     "gender" =>"male"
 ];
-
-$convertToJson = json_encode($person);
+// CONVERT ARRAY TO JSON
+$convertToJson = json_encode($person,JSON_FORCE_OBJECT);
 echo $convertToJson."<hr>";
+// CONVERT JSON TO ARRAY
 $json = '{"name":"diako","email":"diako.dindar@gmail.com","age":37,"gender":"male"}';
 $convertToArray = json_decode($json,true);
-var_dump($convertToArray);
+    echo '<pre>';
+    print_r($convertToArray);
+    echo '</pre>';
 echo $convertToArray['email'];
 echo "<hr>";
 $convertToArray = json_decode($json);
